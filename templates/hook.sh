e198bec7b5a462e3d9405a667768f2881af9cd49
Andrew Simmons <asimmons@waikato.ac.nz>
2021-03-16 16:38:50 +1300

    Add certbot hooks that run for every cert and only read RENEWED_DOMAINS in deploy hook.

--- templates/hook.sh
+++ templates/hook.sh
@@ -2,17 +2,25 @@
 # {{ ansible_managed }}
 
 set -euo pipefail
+{% if item == 'deploy' %}
 
-case "${RENEWED_DOMAINS}" in
+for domain in $RENEWED_DOMAINS; do
+    case "${domain}" in
 {% for domain in certbot_certs %}
-{% if domain[item+'-hook'] is defined %}
-    "{{ domain['domains'][0] }}")
-        {{ domain[item+'-hook'] | indent(8, false) }}
-    ;;
+{% if domain['deploy-hook'] is defined %}
+        "{{ domain['domains'][0] }}")
+            {{ domain['deploy-hook'] | indent(12, false) | trim }}
+        ;;
 {% endif %}
 {% endfor %}
-    *)
-        # noop
-        :
-    ;;
-esac
+        *)
+            # noop
+            :
+        ;;
+    esac
+done
+{% endif %}
+{% if hostvars[inventory_hostname]['certbot_'+item+'_hook'] is defined %}
+
+{{ hostvars[inventory_hostname]['certbot_'+item+'_hook'] | trim }}
+{% endif %}
